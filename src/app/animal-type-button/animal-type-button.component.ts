f53eaeee452fbaa1deab22d82475021a5c503160
Mikael Ruffieux <ruffieux.mikael@gmail.com>
2022-01-19 14:13:03 +0100

    Merging

--- src/app/animal-type-button/animal-type-button.component.ts
+++ src/app/animal-type-button/animal-type-button.component.ts
@@ -1,10 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-<<<<<<< HEAD
 import { Animal } from 'src/models/animal.model';
-=======
-import { Animal } from 'src/models/animal';
-import { Routes, RouterModule } from '@angular/router';
->>>>>>> 91fc5a569e9512c457846272a69bdaf40fbd01b3
 
 @Component({
   selector: 'app-animal-type-button',
